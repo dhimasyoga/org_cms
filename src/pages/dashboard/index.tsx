f8b67cc0c4b941bd2e59ac45e654e979cc923c84
Dhimas Yoga <dhimasyoga146@yahoo.com>
2025-04-28 17:07:33 +0700

    Update Dashboard Copywriting
    Update Dashboard Copywriting

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import { getSession } from 'next-auth/react';
 import { MainLayout } from '@/components/template';
 import { NextPage } from "next";
+import { Box, Typography } from '@mui/material';
+import { theme } from '@/config/themes.config';
 
 export const getServerSideProps = async (context: any) => {
   const { req } = context;
@@ -15,11 +17,27 @@ export const getServerSideProps = async (context: any) => {
   }
 };
 
+const styles = {
+  '.highlight': {
+    color: theme.palette.primary.main
+  },
+  span: {
+    fontWeight: 600,
+  }
+}
+
 const Dashboard: NextPage = () => {
   return (
-    <MainLayout
-      pageTitle='Dashboard'
-    />
+    <MainLayout pageTitle='Dashboard'>
+      <Box component="div" sx={styles}>
+        <Typography variant='h5'>
+          Welcome to <span className="highlight">ORG CMS</span> Dashboard!
+        </Typography>
+        <Typography>
+          Head over to <span>User List</span> on the sidebar to view, edit, and manage your users with ease
+        </Typography>
+      </Box>
+    </MainLayout>
   )
 }
 
